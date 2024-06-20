d7889a7929add08a3b5e582cf8a8be817f46e4cb
Samuel Chacon <sechafer@hotmail.com>
2024-06-19 21:35:15 -0300

    changes  producdetais index src

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,9 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+import { getParam } from "./utils.mjs";
 
+const productId = getParam("product");
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  let cart = getLocalStorage("so-cart") || [];
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  cart.unshift(product);
-  addProductToCart(cart);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
