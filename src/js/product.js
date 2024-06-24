45be396ce4d18dfdfadc8ba559fb7e576d54ce9f
Samuel Chacon <sechafer@hotmail.com>
2024-06-23 21:32:19 -0300

    sch--individual2

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,10 @@
 import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
-import { getParam } from "./utils.mjs";
+import ProductList from "./ProductList.mjs";
+import { updateCartCount } from "./cart";
 
-const productId = getParam("product");
 const dataSource = new ProductData("tents");
+const element = document.querySelector(".product-list");
+const productListing = new ProductList("tents", dataSource, element);
 
-const product = new ProductDetails(productId, dataSource);
-product.init();
+productListing.init();
+updateCartCount();
